80cf0e295a4a63cd3397bee982c9ce7e08c69099
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-03-14 09:33:24 +0000

    Break out separate pkg package
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,10 +1,11 @@
-// Copyright (c) Alex Ellis 2017-2018. All rights reserved.
+// Copyright (c) Alex Ellis 2017-2020. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-package cmd
+package types
 
 // TaskRequest holds options for running a jaas task
 type TaskRequest struct {
+	Verbose     bool
 	Image       string
 	Networks    []string
 	Constraints []string
